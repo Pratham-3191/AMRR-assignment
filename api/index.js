e05ade3286c18e58dbc6909127cd3d285aec896f
Pratham-3191 <prathamchaudhari124@gmail.com>
2025-06-22 16:15:24 +0530

    prodiction fixed

--- api/index.js
+++ api/index.js
@@ -7,9 +7,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL ||'http://localhost:5173',
-  credentials: true
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
 }));
+
+
+app.get('*', (req, res) => {
+  res.status(400).json({ message: 'Bad Request' });
+});
+
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
