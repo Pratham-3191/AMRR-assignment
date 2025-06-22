f040d96c011564b85fecc66880e4af627f28f0b3
Pratham-3191 <prathamchaudhari124@gmail.com>
2025-06-22 16:20:24 +0530

    changed app.listen

--- api/index.js
+++ api/index.js
@@ -1,41 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const serverless = require('serverless-http');
+const app = require('./server'); 
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
-}));
-
-
-app.get('*', (req, res) => {
-  res.status(400).json({ message: 'Bad Request' });
-});
-
-
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-
-const itemRoutes = require('./routes/ItemRoutes.js');
-app.use('/api/items', itemRoutes);
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("DB Connection error:", err));
-
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
-});
+module.exports = serverless(app);
